6ba0532e60a48467545da1a01041eef231c84659
icefo <electro.adrix@gmail.com>
2015-05-18 23:34:53 +0200

    improved code clarity

--- GUI/Qstackedlayout_test.py
+++ GUI/Qstackedlayout_test.py
@@ -20,12 +20,12 @@ class SenderWidget(QWidget):
         self.setLayout(widget_layout)
 
     def hideEvent(self, event):
-        print('hideEvent')
+        print('SenderWidget hideEvent')
         super(SenderWidget, self).hideEvent(event)
         self.signal_hided.emit()
 
     def showEvent(self, event):
-        print('showEvent')
+        print('SenderWidget showEvent')
         super(SenderWidget, self).showEvent(event)
         self.signal_shown.emit()
 
@@ -50,14 +50,16 @@ class ReceiverWidget(QWidget):
         widget_layout.addWidget(self.button, 0, 1)
         self.button.clicked.connect(self.hide)
         self.setLayout(widget_layout)
+        # Hide the widget on init, happen only once
+        self.hide()
 
     def hideEvent(self, event):
-        print('hideEvent')
+        print('ReceiverWidget hideEvent')
         super(ReceiverWidget, self).hideEvent(event)
         self.signal_hided.emit()
 
     def showEvent(self, event):
-        print('showEvent')
+        print('ReceiverWidget showEvent')
         super(ReceiverWidget, self).showEvent(event)
         self.signal_shown.emit()
 
@@ -65,22 +67,24 @@ class ReceiverWidget(QWidget):
 class MainWidget(QWidget):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
-        self.widget1 = SenderWidget()
-        self.widget2 = ReceiverWidget()
+        self.sender_widget = SenderWidget()
+        self.receiver_widget = ReceiverWidget()
 
         # connect signals, so if one widget is hidden then other is shown
-        self.widget1.signal_hided.connect(self.widget2.show)
-        self.widget2.signal_hided.connect(self.widget1.show)
-        self.widget2.signal_shown.connect(self.widget1.hide)
-        self.widget1.signal_shown.connect(self.widget2.hide)
+        self.sender_widget.signal_hided.connect(self.receiver_widget.show)
+        self.receiver_widget.signal_hided.connect(self.sender_widget.show)
 
-        self.widget1.send_text.connect(self.widget2.receive_text)
+        # self.receiver_widget.signal_shown.connect(self.sender_widget.hide)
+        # self.sender_widget.signal_shown.connect(self.receiver_widget.hide)
+
+        self.sender_widget.send_text.connect(self.receiver_widget.receive_text)
 
         # some test code
         layout = QVBoxLayout()
-        layout.addWidget(self.widget1)
-        layout.addWidget(self.widget2)
+        layout.addWidget(self.sender_widget)
+        layout.addWidget(self.receiver_widget)
         self.setLayout(layout)
+
 
 
 if __name__ == "__main__":
