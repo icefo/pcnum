0519ace5450023964b1f57e32d9312dccb69098f
icefo <electro.adrix@gmail.com>
2015-07-19 01:53:05 +0200

    sethtml is not the way to go, I'll ty to qtableview instead

--- GUI/search/ResultWidget.py
+++ GUI/search/ResultWidget.py
@@ -1,8 +1,10 @@
 __author__ = 'adrien'
 
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QGridLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QGridLayout, QTextBrowser
 
+# use html settext fo the Qtextedit module
+# mauvaise idee, utilise qtableview---> pas editable mais c'est pas grave
 
 class ResultWidget(QWidget):
     show_search_widget_signal = QtCore.pyqtSignal()
@@ -13,11 +15,58 @@ class ResultWidget(QWidget):
         widget_layout = QGridLayout()
         self.setLayout(widget_layout)
 
-        line_edit = QLabel()
+        textwidget = QTextBrowser()
+        html = """
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+*
+{
+    margin: 0px;
+    padding: 0px;
+}
+html, body
+{
+    height: 100%;
+}
+</style>
+</head>
+
+<body>
+
+<table border="1" style="width:100%;height:100%;">
+  <tr>
+    <td>Jill</td>
+    <td>
+<ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>
+    </td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+  <tr>
+    <td>John</td>
+    <td>Doe</td>
+    <td>80</td>
+  </tr>
+</table>
+
+</body>
+</html>
+"""
+        textwidget.setHtml(html)
         # self.receive_text.connect(line_edit.setText)
-        widget_layout.addWidget(line_edit, 0, 0)
+        widget_layout.addWidget(textwidget, 0, 0, 3, 2)
         self.button = QPushButton('Back')
-        widget_layout.addWidget(self.button, 0, 1)
+        widget_layout.addWidget(self.button, 4, 2)
         self.button.clicked.connect(self.show_search_widget_signal.emit)
         self.receive_list.connect(self.search_done)
 
