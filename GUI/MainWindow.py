6092eb5fb6e2727124e4aeac7af03033b031ebcc
icefo <electro.adrix@gmail.com>
2015-10-20 02:05:28 +0200

    It's now possible to call and get the result from a wamp procedure with a pushbutton that live in an Qwidget subclass and not only from the QMainWindow class.
    The signals that I used to achieve that add some boilerplate. I'll see if I can do better later today.

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -79,4 +79,3 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainWindow()
     sys.exit(app.exec_())
-
--- wamp/gui_client.py
+++ wamp/gui_client.py
@@ -2,7 +2,8 @@ __author__ = 'adrien'
 
 import sys
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QLineEdit, QGridLayout
 
 import setproctitle
 from autobahn.asyncio.wamp import ApplicationRunner
@@ -19,6 +20,8 @@ class MainWindow(ApplicationSession, QMainWindow):
         ApplicationSession.__init__(self, config)
         QMainWindow.__init__(self)
 
+        self.the_widget = MainWidget()
+
         #########
         self.main_window_init()
 
@@ -28,30 +31,54 @@ class MainWindow(ApplicationSession, QMainWindow):
         try:
             res = yield from self.call('com.myapp.add2', 6, 3)
             print("call result: {}".format(res))
-            self.buttonpushed()
         except Exception as e:
             print("call error: {0}".format(e))
 
     def main_window_init(self):
 
-        button = QPushButton("push push push !")
-        button.clicked.connect(self.buttonpushed)
-        self.setCentralWidget(button)
+        self.the_widget.push_button_clicked.connect(self.main_widget_button_clicked)
+        self.setCentralWidget(self.the_widget)
 
         #########
-        self.setGeometry(300, 300, 200, 200)
+        self.setGeometry(300, 300, 300, 200)
         self.setWindowTitle('gui_wamp_test')
         self.show()
 
-    def buttonpushed(self):
+    def main_widget_button_clicked(self):
 
         @coroutine
         def call_it_baby():
-            print("he pushed it !")
+            print("call it baby !")
             res = yield from self.call('com.myapp.add2', 6, 4)
-            print(res)
+            print("I called baby ! ", res)
+
+            self.the_widget.push_button_answer.emit(str(res))
 
         asyncio.async(call_it_baby())
+
+
+class MainWidget(QWidget):
+
+    push_button_clicked = pyqtSignal()
+    push_button_answer = pyqtSignal([str])
+
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.widget_init()
+
+    def widget_init(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        push_button = QPushButton("push push push !")
+        push_button.clicked.connect(self.push_button_clicked)
+
+        line_edit = QLineEdit()
+        self.push_button_answer.connect(line_edit.setText)
+
+        grid.addWidget(push_button, 0, 0)
+        grid.addWidget(line_edit, 1, 0)
 
 if __name__ == "__main__":
     qapp = QApplication(sys.argv)
