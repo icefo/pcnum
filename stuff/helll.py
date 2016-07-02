c510694df97a1c06682ffd2f1fb2a84550ccc2fe
icefo <electro.adrix@gmail.com>
2016-07-02 18:44:58 +0200

    cleanup before merging to master

--- stuff/helll.py
+++
@@ -1,6 +0,0 @@
-di = {"title": 0, "year": 1896, "dc:identifier": '65293c71-cbc4-4ab0-9038-eaa51522912f',
-                                       "start_date": "16:86:96", "source": "vhs", "action": "digitise", "progress": 22,
-                                       "date_data_send": 1}
-
-for a in di.keys():
-    print(a)
